3c2fc58010d7f06db7f187627413a329d1e53502
Vitor Figueiredo <vittor.figueiredo17@gmail.com>
2023-09-15 16:48:29 -0300

    Primeiro commit

---
+++ src/main/java/com/vitorfigueired0/taskforge/TaskForgeApplication.java
@@ -0,0 +1,13 @@
+package com.vitorfigueired0.taskforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskForgeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TaskForgeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/vitorfigueired0/taskforge/TaskForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorfigueired0.taskforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskForgeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
